15738cfa8736daa4e9526cc5f6cd39af6ee02d0f
Enyell Duarte <enyelldev@gmail.com>
2025-05-16 21:04:53 -0400

    Loqueras hechas

---
+++ app/core/Database.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Core;
+
+class Database
+{
+    private static $instance = null;
+    private $pdo;
+
+    private function __construct()
+    {
+        $config = require __DIR__ . '/../../config/database.php';
+        
+        try {
+            $this->pdo = new \PDO(
+                "mysql:host={$config['host']};dbname={$config['dbname']};charset=utf8",
+                $config['user'],
+                $config['password'],
+                [
+                    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+                    \PDO::ATTR_EMULATE_PREPARES => false
+                ]
+            );
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection()
+    {
+        return $this->pdo;
+    }
+
+    // Evita la clonación del singleton
+    private function __clone() {}
+    private function __wakeup() {}
+}
