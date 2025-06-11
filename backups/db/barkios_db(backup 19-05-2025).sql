8f12066d7d695350e8fafad4aa7d07d86f667993
Nokx1z <enyelldev@gmail.com>
2025-06-11 18:01:55 -0400

    Se cargo el backup con compatibilidad a versiones anteriores a la 8.0 de MySQL

--- backups/db/barkios_db(backup 19-05-2025).sql
+++ backups/db/barkios_db(backup 19-05-2025).sql
@@ -1,21 +1,17 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.2
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: barkios-db
 -- Tiempo de generación: 19-05-2025 a las 18:08:40
--- Versión del servidor: 8.0.42
--- Versión de PHP: 8.2.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de datos: `barkios_db`
@@ -33,7 +29,7 @@ CREATE TABLE `clientes` (
   `direccion` varchar(255) DEFAULT NULL,
   `telefono` varchar(20) DEFAULT NULL,
   `membresia` varchar(10) NOT NULL DEFAULT 'regular'
-) ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `clientes`
@@ -56,7 +52,7 @@ CREATE TABLE `productos` (
   `precio` decimal(10,2) NOT NULL,
   `imagen` varchar(255) DEFAULT NULL,
   `descripcion` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -65,12 +61,12 @@ CREATE TABLE `productos` (
 --
 
 CREATE TABLE `proveedores` (
-  `id` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'RIF del proveedor',
-  `nombre_contacto` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nombre_empresa` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `direccion` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tipo_rif` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Tipo de RIF: J (Jurídico), G (Gubernamental), C (Cooperativa)'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `id` varchar(12) NOT NULL COMMENT 'RIF del proveedor',
+  `nombre_contacto` varchar(100) NOT NULL,
+  `nombre_empresa` varchar(100) NOT NULL,
+  `direccion` text NOT NULL,
+  `tipo_rif` char(1) NOT NULL COMMENT 'Tipo de RIF: J (Jurídico), G (Gubernamental), C (Cooperativa)'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `proveedores`
